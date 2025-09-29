3c1debb29b332e7646876fb20a6a4f2000fabd07
Epril Purwandi <mailbox.epril@gmail.com>
2025-09-29 10:36:40 +0000

    firebase deploy
    === Deploying to 'studio-5403298991-e6700'...
    
    i  deplo

--- src/lib/midtrans.ts
+++
@@ -1,8 +0,0 @@
-import midtransClient from 'midtrans-client';
-
-// Inisialisasi Midtrans Snap
-export const snap = new midtransClient.Snap({
-    isProduction: false, // Ganti ke true saat production
-    serverKey: process.env.MIDTRANS_SERVER_KEY,
-    clientKey: process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY
-});
