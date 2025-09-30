26166a7ad0c729debef345edfe3e2bdbe3e19d5e
Epril Purwandi <mailbox.epril@gmail.com>
2025-09-30 03:50:07 +0000

    kamu bisa analisa gagal publish

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,6 @@
-require('dotenv').config({ path: './.env' });
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config({ path: './.env' });
+}
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
