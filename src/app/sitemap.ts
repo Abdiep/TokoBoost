396bcd6b66b0840dd0500d9b7b01f86bb1aa83b9
Epril Purwandi <mailbox.epril@gmail.com>
2025-10-14 10:21:51 +0000

    kenapa di google search console tidak terbaca sitemapnya

---
+++ src/app/sitemap.ts
@@ -0,0 +1,36 @@
+import { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://tokoboost.com',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://tokoboost.com/tentang-kami',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://tokoboost.com/kontak-kami',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+     {
+      url: 'https://tokoboost.com/privasi',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.3,
+    },
+     {
+      url: 'https://tokoboost.com/syarat-dan-ketentuan',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.3,
+    },
+  ]
+}
